c8dc8ed6c9a000ef49d82a73027559bd592f5ae7
ShiyuXie <sy.xie.jobs@gmail.com>
2019-07-26 10:35:38 +1000

    Add the project structure

---
+++ src/test/java/com/dustyfingers/carms/CarMsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dustyfingers.carms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CarMsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
