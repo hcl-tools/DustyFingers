74d9cf8de1c8fcf69cf75d26fc183314e5293187
domred <domredan@gmail.com>
2019-07-26 12:21:10 +1000

    implemented car service for car operations

--- src/main/java/com/dustyfingers/CarMS/service/CarServiceImpl.java
+++ src/main/java/com/dustyfingers/CarMS/service/CarServiceImpl.java
@@ -1,28 +1,51 @@
 package com.dustyfingers.CarMS.service;
 
+import com.dustyfingers.CarMS.dao.AuditRepository;
+import com.dustyfingers.CarMS.dao.CarRepository;
 import com.dustyfingers.CarMS.model.Car;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class CarServiceImpl {
-    Car makeCar(Car car) {
-        return new Car();
+
+    @Autowired
+    CarRepository carRepository;
+
+    @Autowired
+    AuditRepository auditRepository;
+
+    Car makeCar(Car car) throws Exception {
+        return carRepository.save(car);
     }
 
-    Car findCarById(int id) {
-        return new Car();
+    Car findCarById(int id) throws Exception {
+        return carRepository.findById(id)
+                .orElseThrow(() -> new Exception("Car not found"));
     }
 
     List<Car> findAllCars() {
-        return Arrays.asList(new Car());
+        Iterable<Car> cars = carRepository.findAll();
+        if(cars instanceof List) {
+            return (List<Car>) cars;
+        }
+        ArrayList<Car> list = new ArrayList<>();
+        if(cars != null) {
+            for(Car c: cars) {
+                list.add(c);
+            }
+        }
+        return list;
     }
 
-    Car updateCar(Car car) {
-        return new Car();
+    Car updateCar(Car car) throws Exception {
+        return carRepository.save(car);
     }
 
-    boolean deleteCar(int id) {
+    boolean deleteCar(int id) throws Exception {
         return false;
     }
 }
